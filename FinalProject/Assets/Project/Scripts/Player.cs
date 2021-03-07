7d7f2cd6ddc2a8420e9149d732cfae401b8aabd9
k7795@office365svip.tw <k7795@office365svip.tw>
2021-03-07 14:42:07 -0800

    Walking and Jumping of player
    Walking and Jumping of player

--- FinalProject/Assets/Project/Scripts/Player.cs
+++ FinalProject/Assets/Project/Scripts/Player.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [Header("Movement")]
+    public float walkingSpeed;
+    public float jumpingSpeed;
+
+    private Rigidbody2D playerRigidbody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRigidbody = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // TO MAKE THE PLAYER MOVE HORIZAONTALLY
+        playerRigidbody.velocity = new Vector2(
+                 Input.GetAxis("Horizontal") * walkingSpeed,             //GET AXIS RETURNS -1 TO 1
+                 playerRigidbody.velocity.y
+             );
+        //TO MAKE THE PLAYER JUMP
+        if (Input.GetAxis("Jump") > 0)
+        {
+            playerRigidbody.velocity = new Vector2(
+                    playerRigidbody.velocity.x,
+                    jumpingSpeed
+                );
+        }
+            
     }
 }
