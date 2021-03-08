ee8c29f85fbd390a3a637419adb8c85977821939
pujithasak <pujithasak@gmail.com>
2021-03-07 21:08:38 -0600

    Added Mario Player animations

--- FinalProject/Assets/Project/Scripts/Player.cs
+++ FinalProject/Assets/Project/Scripts/Player.cs
@@ -8,13 +8,19 @@ public class Player : MonoBehaviour
     public float walkingSpeed;
     public float jumpingSpeed;
 
+    [Header("Visuals")]
+    public GameObject container;
+    public float powerupScale = 1.3f;
+
     private Rigidbody2D playerRigidbody;
+    private Animator playerAnimator;
     private float height;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRigidbody = gameObject.GetComponent<Rigidbody2D>();
+        playerAnimator = gameObject.GetComponent<Animator>();
 
         height = gameObject.GetComponent<CapsuleCollider2D>().size.y;
     }
@@ -44,9 +50,20 @@ public class Player : MonoBehaviour
                  Input.GetAxis("Horizontal") * walkingSpeed,             //GET AXIS RETURNS -1 TO 1
                  playerRigidbody.velocity.y
              );
+
+        // Point the player towards the right direction.
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            container.transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (Input.GetAxis("Horizontal") < 0)
+        {
+            container.transform.localScale = new Vector3(-1, 1, 1);
+        }
+
         //TO MAKE THE PLAYER JUMP
-       // RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, ( height / 2) + 0.1f);
-       // Debug.Log(hit.collider.name);
+        // RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, ( height / 2) + 0.1f);
+        // Debug.Log(hit.collider.name);
         RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y - height / 2 - 0.01f), Vector2.down, 0.02f);
 
         if (hit.collider != null &&  Input.GetAxis("Jump") > 0)
@@ -56,6 +73,10 @@ public class Player : MonoBehaviour
                     jumpingSpeed
                 );
         }
-            
+
+        // Set the player's animation.
+        playerAnimator.SetBool("Run", Mathf.Abs(playerRigidbody.velocity.x) > 0);
+        playerAnimator.SetBool("Jump", hit.collider == null);
+
     }
 }
